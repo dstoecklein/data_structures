0c15f7ccd0a0c4ea8c92cec03c72ea967f646856
Daniel Stoecklein <daniel.stoecklein@gmail.com>
2022-03-17 17:54:08 +0100

    print linked list

--- linked_list/node.py
+++ linked_list/node.py
@@ -1,3 +1,6 @@
+from traceback import print_last
+
+
 class Node():
     def __init__(self, data) -> None:
         self.data = data
@@ -6,16 +9,24 @@ class Node():
     def __repr__(self) -> str:
         return f'{self.data}'
 
+def print_lst(head):
+    while head is not None:
+        print(head.data, end='->')
+        head = head.next
+    print()
+
+def print_rec(head):
+    if head is not None:
+        print(head.data, end='->')
+        print_rec(head.next)
 
 if __name__ == "__main__":
     node1 = Node(1)
     node2 = Node(2)
+    node3 = Node(3)
 
     node1.next = node2
+    node2.next = node3
 
-    print(node1)
-    print(node2)
-
-    # check link
-    print(id(node2))
-    print(id(node1.next))
+    print_lst(node1)
+    print_rec(node1)
